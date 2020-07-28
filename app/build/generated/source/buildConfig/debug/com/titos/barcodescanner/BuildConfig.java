b8c9164f378ed049cace7fe5f51606e5e18672ec
nikhil <nikhilbadveli6@gmail.com>
2020-07-28 11:04:28 +0530

    Rebranded to Riki Stores

--- app/build/generated/source/buildConfig/debug/com/titos/barcodescanner/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/titos/barcodescanner/BuildConfig.java
@@ -5,8 +5,8 @@ package com.titos.barcodescanner;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.titos.barcodescanner";
+  public static final String APPLICATION_ID = "com.titos.rikistores";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 8;
-  public static final String VERSION_NAME = "2.0.1";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
 }
